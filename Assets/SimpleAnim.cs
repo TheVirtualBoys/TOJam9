39cf38eab01e3f3c047ba94be52c43029cf5cd66
Tim McLennan <tim@nothingness.ca>
2014-04-25 19:23:09 -0400

    simple stuff

---
+++ Assets/SimpleAnim.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SimpleAnim : MonoBehaviour
+{
+	List<Vector3> animFrames = new List<Vector3>();
+	float t                  = 0.0f;
+	int thisFrame            = 0;
+
+	public void SetAnim(List<Vector3> frames)
+	{
+		animFrames = frames;
+		t          = 0.0f;
+		thisFrame  = 0;
+		if (animFrames.Count > 0)
+		{
+			//Utils.SetTexture(new Vector2(x, y), new Vector2(rows, cols), gameObject);
+		}
+	}
+
+	void Update()
+	{
+		// Advance animation.
+		if (0 == animFrames.Count) return;
+		t += Time.deltaTime;
+		if (0 < animFrames[thisFrame].z && t >= animFrames[thisFrame].z)
+		{
+			while (t >= animFrames[thisFrame].z)
+			{
+				t -= animFrames[thisFrame].z;
+				++thisFrame;
+				if (thisFrame >= animFrames.Count)
+				{
+					thisFrame = 0;
+				}
+			}
+			//Utils.SetTexture(new Vector2(x, y), new Vector2(rows, cols), gameObject);
+		}
+	}
+}
---
+++ Assets/SimpleSprite.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleSprite : MonoBehaviour
+{
+	MeshRenderer renderer = null;
+	MeshFilter mesh = null;
+	
+	void Start()
+	{
+		mesh = new MeshFilter();
+		mesh.mesh = CreateQuad();
+	}
+
+	Mesh CreateQuad()
+	{
+		Mesh mesh = new Mesh();
+
+		Vector3[] verts = new Vector3[] {
+			new Vector3(0, 0, 0),
+			new Vector3(1, 0, 0),
+			new Vector3(0, 1, 0),
+			new Vector3(1, 1, 0)
+		};
+
+		Vector2[] uvs = new Vector2[] {
+			new Vector2(1, 1),
+			new Vector2(1, 0),
+			new Vector2(0, 1),
+			new Vector2(0, 0)
+		};
+
+		int[] tris = new int[] {
+			0, 1, 2,
+			2, 1, 3
+		};
+
+		mesh.vertices = verts;
+		mesh.uv = uvs;
+		mesh.triangles = tris;
+		return mesh;
+	}
+
+	void SetMaterial()
+	{
+	}
+}
