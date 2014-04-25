8275d00faa3026c0f2bf98697e6e4393b25973dc
Tim McLennan <tim@nothingness.ca>
2014-04-25 17:18:33 -0400

    Line endings, func to set atlas position

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -4,15 +4,19 @@ using System.Collections.Generic;
 
 public class Main : MonoBehaviour
 {
+	void Awake()
+	{
+		
+	}
 	// Use this for initialization
 	void Start()
 	{
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update()
 	{
-	
+		
 	}
 }
---
+++ Assets/Utils.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public static class Utils
+{
+	public static void SetTexture(Vector2 coord, Vector2 rowscols, GameObject rendererGO)
+	{
+		SetBadTexture(coord, rowscols, rendererGO);
+	}
+
+	public static void SetBadTexture(Vector2 coord, Vector2 rowscols, GameObject rendererGO)
+	{
+		Vector2 scale  = new Vector2(1.0f / rowscols.x, 1.0f / rowscols.y); // Assuming square sprite.
+		Vector2 offset = new Vector2(scale.x * coord.x, 1 - ((scale.y * coord.y) + scale.y));
+
+		if (rendererGO)
+		{
+		/**
+		 * This is bad because it creates an instance of the material assigned to the rendererGO's meshfilter
+		 */
+			rendererGO.renderer.material.mainTextureScale  = scale;
+			rendererGO.renderer.material.mainTextureOffset = offset;
+		}
+	}
+}
