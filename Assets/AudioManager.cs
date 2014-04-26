b4fab22f325b32527213d6af63122c04ccb9750a
Tim McLennan <tim@nothingness.ca>
2014-04-26 17:30:00 -0400

    Finish up most of Audiomanager. need to add in hooks for animation stuff.

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -5,13 +5,20 @@ using System.Collections.Generic;
 public class AudioManager
 {
 	//Dictionary<Tracks, List<AudioSource> > sources = 
-	List<AudioSource> sources   = new List<AudioSource>();
-	AudioClip[] tracks          = new AudioClip[(int)Tracks.TRACK_MAX];
-	int[] trackPosition         = new int[(int)Tracks.TRACK_MAX];
-	int[] trackOffsetMax        = new int[(int)Tracks.TRACK_MAX];
-	float[] trackPitchOffsetMax = new float[(int)Tracks.TRACK_MAX];
-	float t                     = 0.0f;
-	int numOffsetDudes          = 0;
+	List<AudioSource> sources       = new List<AudioSource>();
+	AudioClip[] tracks              = new AudioClip[(int)Tracks.TRACK_MAX];
+	float[] trackPitchOffsetMax     = new float[(int)Tracks.TRACK_MAX];
+	float[] trackOffsetMax          = new float[(int)Tracks.TRACK_MAX];
+	float[] trackPosition           = new float[(int)Tracks.TRACK_MAX];
+	int[] numOffsetDudes            = new int[(int)Tracks.TRACK_MAX];
+	List<AudioSource> offsetSources = new List<AudioSource>();
+	float t                         = 0.0f;
+	float lastOffsetTime            = 0.0f;
+	const int maxOffsetDudes          = 1;
+	const float maxTimeBetweenOffsets = 20.0f;
+	const float minTimeBetweenOffsets = 7.0f;
+	const float percentageChance      = 40.0f;
+	const float pitchOffset           = 0.01f;
 
 	public enum Tracks
 	{
@@ -30,16 +37,36 @@ public class AudioManager
 		tracks[3] = Resources.Load<AudioClip>("Flute_Loop");
 		for (int i = 0; i < (int)Tracks.TRACK_MAX; ++i)
 		{
-			trackPosition[i]       = 0;
-			trackOffsetMax[i]      = 44100; // 44.1kHz == 1s
-			trackPitchOffsetMax[i] = 0.02f;
+			trackPosition[i]       = 0.0f;
+			trackOffsetMax[i]      = 1.0f; // 2 seconds
+			trackPitchOffsetMax[i] = 0.10f;
 		}
+	}
+
+	int TotalOffsetDudes()
+	{
+		int num = 0;
+		for (int i = 0; i < (int)Tracks.TRACK_MAX; ++i)
+		{
+			num += numOffsetDudes[i];
+		}
+		return num;
+	}
+
+	Tracks GetTrack(AudioSource source)
+	{
+		for (int i = 0; i < (int)Tracks.TRACK_MAX; ++i)
+		{
+			if (source.clip == tracks[i]) return (Tracks)i;
+		}
+		return Tracks.TRACK_MAX;
 	}
 
 	public void CreateAudioSource(AudioSource source, Tracks track)
 	{
-		source.clip        = tracks[(int)track];
-		source.timeSamples = trackPosition[(int)track];
+		source.clip = tracks[(int)track];
+		source.time = trackPosition[(int)track];
+		source.loop = true;
 		sources.Add(source);
 	}
 
@@ -55,7 +82,107 @@ public class AudioManager
 
 	public void Update()
 	{
-		// put logic for getting people out of sync.
+		float dt = Time.deltaTime;
+		t += dt;
+		for (int i = 0; i < (int)Tracks.TRACK_MAX; ++i)
+		{
+			trackPosition[i] += dt;
+		}
+		int offsetDudes = TotalOffsetDudes();
 
+		if (offsetDudes < maxOffsetDudes && t >= lastOffsetTime + minTimeBetweenOffsets)
+		{
+			// we can offset a dude!
+			if (t >= lastOffsetTime + maxTimeBetweenOffsets || Random.Range(0, 100) < percentageChance)
+			{
+				// desync a dude!!
+				// 1. find which instrument to desync
+				List<int> offsetableTracks = new List<int>();
+				for (int i = 0; i < (int)Tracks.TRACK_MAX; ++i)
+				{
+					if (numOffsetDudes[i] == 0)
+					{
+						offsetableTracks.Add(i);
+					}
+				}
+				int track = -1;
+				if (offsetableTracks.Count == 0)
+				{
+					track = Random.Range(0, (int)Tracks.TRACK_MAX);
+				}
+				else
+				{
+					track = Random.Range(0, offsetableTracks.Count);
+				}
+				
+				// 2. pick a random dude playing that instrument to desync
+				List<AudioSource> trackDudes = new List<AudioSource>();
+				for (int i = 0; i < sources.Count; ++i)
+				{
+					if (sources[i].clip == tracks[track] && !offsetSources.Contains(sources[i]))
+					{
+						trackDudes.Add(sources[i]);
+					}
+				}
+
+				if (trackDudes.Count > 0)
+				{
+					int sourceIndex = -1;
+					AudioSource source = null;
+					while (sourceIndex == -1)
+					{
+						sourceIndex = Random.Range(0, trackDudes.Count - 1);
+						source = trackDudes[sourceIndex];
+						if (offsetSources.Contains(source))
+						{
+							sourceIndex = -1;
+						}
+					}
+
+					AudioSource screwWithThisOne = trackDudes[sourceIndex];
+					// 3. mess with its pitch
+					float offset = Random.Range(pitchOffset, trackPitchOffsetMax[(int)track]);
+					if ((screwWithThisOne.timeSamples & 1) == 1)
+					{
+						screwWithThisOne.pitch += offset;
+					}
+					else
+					{
+						screwWithThisOne.pitch -= offset;
+					}
+					Debug.Log("Offset " + source + " @" + offset);
+					offsetSources.Add(source);
+					++numOffsetDudes[track];
+					lastOffsetTime = t;
+				}
+			}
+		}
+
+		// 4. keep track until it's too offset then reset the pitch
+		for (int i = 0; i < offsetSources.Count; ++i)
+		{
+			AudioSource source = offsetSources[i];
+			Tracks track = GetTrack(source);
+			if (track == Tracks.TRACK_MAX) continue;
+
+			if (source.time >= (trackPosition[(int)track] + trackOffsetMax[(int)track]) || source.time <= (trackPosition[(int)track] - trackOffsetMax[(int)track]))
+			{
+				if (source.pitch != 1.0f) Debug.Log("done offsetting " + offsetSources[i]);
+				source.pitch = 1.0f;
+				// do something with the animation
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			foreach (AudioSource source in offsetSources)
+			{
+				Tracks track = GetTrack(source);
+				source.pitch = 1.0f;
+				source.time  = trackPosition[(int)track];
+				offsetSources.Remove(source);
+				--numOffsetDudes[(int)track];
+			}
+		}
 	}
 }
