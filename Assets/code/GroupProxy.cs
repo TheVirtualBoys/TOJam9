f47d38b0a8ce9d06323f59b83e26940a4e73bd58
cregnier <cregnier@hotmail.com>
2014-04-26 02:40:14 -0400

    - adding some temp quads and flat colours to distinguish each as a separate character - creating a GroupProxy object that will proxy it's position and an animated proxy position for 3 sub characters. This allows easy animated movement for different types of movements that can be reused. - setting up a temp basic animation that moves the children of the ForwardProxy object forward and also back and forth a bit.

---
+++ Assets/code/GroupProxy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroupProxy : MonoBehaviour {
+
+	public Transform character1;
+	public Transform character2;
+	public Transform character3;
+
+	public Vector3 char1Pos;
+	public Vector3 char2Pos;
+	public Vector3 char3Pos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		character1.transform.position = transform.position + char1Pos;
+		character2.transform.position = transform.position + char2Pos;
+		character3.transform.position = transform.position + char3Pos;
+	}
+}
