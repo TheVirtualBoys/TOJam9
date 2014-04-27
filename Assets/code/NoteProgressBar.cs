0d61c25127b2509740e741a1c7b82655917b8d99
cregnier <cregnier@hotmail.com>
2014-04-27 18:01:29 -0400

    - updating the note progress bar a bit so it does the win state when it hits 100%

--- Assets/code/NoteProgressBar.cs
+++ Assets/code/NoteProgressBar.cs
@@ -18,24 +18,105 @@ public class NoteProgressBar : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		noteSpriteWidth = note.GetComponent<SpriteRenderer>().bounds.extents.x * 2;
+
+		//aniToTrack.Play("FieldScrollingAnim", -1, 0.9f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float normalizedTime = aniToTrack.GetCurrentAnimatorStateInfo(0).normalizedTime;
-		float aniCompletion = normalizedTime - Mathf.Floor(normalizedTime);
+		if (percentComplete < 1f)
+		{
+			float normalizedTime = aniToTrack.GetCurrentAnimatorStateInfo(0).normalizedTime;
+			float aniCompletion = normalizedTime - Mathf.Floor(normalizedTime);
 
-		percentComplete = aniCompletion;
-		if (percentComplete < 0)
-			percentComplete = 0;
-		if (percentComplete > 1)
-			percentComplete = 1;
+			if (aniCompletion < percentComplete)
+			{
+				Debug.Log("ani: " + aniCompletion + ", %: " + percentComplete);
 
-		float maxWidth = filler.localScale.x - note.localScale.x * noteSpriteWidth;
-		
-		Vector3 notePos = note.position;
-		notePos.x = filler.position.x + percentComplete * maxWidth;
-		notePos.y = filler.position.y + maxBounce * Mathf.Sin(percentComplete * 2 * Mathf.PI * freq);
-		note.position = notePos;
+				//for some reason the following animation is before the current % complete, so it must've looped, so consider % complete done now
+				percentComplete = 1;
+			}
+			else
+			{
+				percentComplete = aniCompletion;
+				if (percentComplete < 0)
+					percentComplete = 0;
+				if (percentComplete >= 1)
+				{
+					percentComplete = 1;
+				}
+			}
+
+			float maxWidth = filler.localScale.x - note.localScale.x * noteSpriteWidth;
+
+			Vector3 notePos = note.position;
+			notePos.x = filler.position.x + percentComplete * maxWidth;
+			notePos.y = filler.position.y + maxBounce * Mathf.Sin(percentComplete * 2 * Mathf.PI * freq);
+			note.position = notePos;
+
+			if (percentComplete >= 1)
+			{
+				OnWin();
+			}
+		}
 	}
+
+	void OnWin()
+	{
+		Transform[] characters = null;
+
+		GameObject mainCamera = GameObject.Find("Main Camera");
+		if (mainCamera != null)
+		{
+			CharacterCollection col = mainCamera.GetComponent<CharacterCollection>();
+			if (col != null)
+			{
+				characters = col.characters;
+			}
+		}
+
+		if (characters != null)
+		{
+			foreach (Transform character in characters)
+			{
+				playAnimation(character.gameObject, false);
+				showWinAnimation(character.gameObject);
+			}
+		}
+
+		GameObject field = GameObject.Find("Field");
+		if (field != null)
+		{
+			Animator ani = field.GetComponent<Animator>();
+			ani.enabled = false;
+		}
+
+		CameraFade.StartAlphaFade(Color.black, false, 5.0f, 0f, () => { RestartScene(); });
+
+	}
+
+	private void playAnimation(GameObject character, bool running)
+	{
+		if (character != null)
+		{
+			AniStrip ani = character.GetComponent<AniStrip>();
+			ani.running = running;
+		}
+	}
+
+	private void showWinAnimation(GameObject character)
+	{
+		WinLoseSpriteSwapper spriteSwapper = character.GetComponent<WinLoseSpriteSwapper>();
+		if (spriteSwapper != null)
+		{
+			spriteSwapper.OnWin();
+		}
+	}
+
+	private void RestartScene()
+	{
+		AudioScript.Reset();
+		Application.LoadLevel("Title");
+	}
+
 }
